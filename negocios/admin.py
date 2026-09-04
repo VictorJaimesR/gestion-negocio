fb9cd51ef243d43524c55279dfe0726589d153f9
Victor Jaimes <maskerre0001@gmail.com>
2026-09-03 22:19:41 -0500

    Agrega modelo Inmueble con registro en admin

--- negocios/admin.py
+++ negocios/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Persona
+from .models import Persona, Inmueble
 
 
 admin.site.register(Persona)
+admin.site.register(Inmueble)
+
 
--- negocios/models.py
+++ negocios/models.py
@@ -9,4 +9,35 @@ class Persona(models.Model):
 
     def __str__(self):
         return self.nombre
-    
+
+class Inmueble(models.Model):
+    TIPO_VEHICULO = 'vehiculo'
+    TIPO_APARTAMENTO = 'apartamento'
+    TIPO_CASA = 'casa'
+    TIPO_MOTOCICLETA = 'motocicleta'
+    TIPO_LOTE = 'lote'
+    TIPO_OTRO = 'otro'
+    TIPO_CHOICES = [
+        (TIPO_VEHICULO, 'Vehículo'),
+        (TIPO_APARTAMENTO, 'Apartamento'),
+        (TIPO_CASA, 'Casa'),
+        (TIPO_MOTOCICLETA, 'Motocicleta'),
+        (TIPO_LOTE, 'Lote'),
+        (TIPO_OTRO, 'Otro'),
+    ]
+
+    ESTADO_DISPONIBLE = 'disponible'
+    ESTADO_VENDIDO = 'vendido'
+    ESTADO_ARRENDADO = 'arrendado'
+    ESTADO_CHOICES = [
+        (ESTADO_DISPONIBLE, 'Disponible'),
+        (ESTADO_VENDIDO, 'Vendido'),
+        (ESTADO_ARRENDADO, 'Arrendado'),
+    ]
+
+    tipo = models.CharField(max_length=20, choices=TIPO_CHOICES)
+    descripcion = models.TextField()
+    estado = models.CharField(max_length=20, choices=ESTADO_CHOICES, default=ESTADO_DISPONIBLE)
+
+    def __str__(self):
+        return f"{self.get_tipo_display()} - {self.descripcion}"
